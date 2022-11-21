7024f82e30a4fe704faa0f936d15bc64922142ce
Hitoki <phitoki@outlook.fr>
2022-11-21 17:14:36 +0100

    Update code.js
    Added fox + 404 + StackOverflow

--- code.js
+++ code.js
@@ -1,20 +1,35 @@
 // https://www.google.com/search?channel=fs&client=ubuntu&q=
 console.log("Script loaded !");
+document.getElementById("inputtext").autofocus = true;
 document.getElementById("inputtext").value = "";
 document.getElementById("inputtext").addEventListener("keyup", ({ key }) => {
   if (key === "Enter") {
     var search = document.getElementById("inputtext").value.toString();
     if (search.startsWith(":")) {
       search = search.toLowerCase();
-      fetch("./shortcut.json")
-        .then((data) => data.json())
-        .then((data) => {
-          for (i in data) {
-            if (i == search.slice(1)) {
-              document.location.href = data[i];
+      if (search.slice(1) == "fox") {
+        fetch("https://randomfox.ca/floof/")
+          .then((data) => data.json())
+          .then((data) => {
+            document.location.href = data.image;
+          });
+      } else {
+        fetch("./shortcut.json")
+          .then((data) => data.json())
+          .then((data) => {
+            for (i in data) {
+              if (i == search.slice(1)) {
+                document.location.href = data[i];
+                break;
+              } else {
+                document.location.href = "https://http.cat/404.jpg";
+              }
             }
-          }
-        });
+          });
+      }
+    } else if (search.startsWith("->")) {
+      document.location.href =
+        "https://stackoverflow.com/search?q=" + search.slice(3);
     } else if (search.startsWith("https://") || search.startsWith("http://")) {
       document.location.href = document.getElementById("inputtext").value;
     } else {
@@ -46,3 +61,4 @@ document.body.addEventListener("keyup", ({ key }) => {
   }
   console.log(compteur);
 });
+
