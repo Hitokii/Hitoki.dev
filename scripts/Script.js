c39fe82c5d19fb1225269ec75b91ed88cf1d022e
Hitoki <phitoki@outlook.fr>
2023-10-01 16:50:13 +0200

    commit

--- scripts/Script.js
+++ scripts/Script.js
@@ -1,20 +1,35 @@
-var sidebar = document.getElementsByClassName("sidebar")[0];
-sidebar.addEventListener("mouseover", function() {
-    sidebar.classList.add("sidebar-hover");
-    });
-sidebar.addEventListener("mouseout", function() {
-    sidebar.classList.remove("sidebar-hover");
-    }
-);
+let themeswitch = document.querySelector('#themeswitcher');
 
-var flags = document.querySelectorAll(".flag");
-flags.forEach(element => {
-    element.addEventListener("click", function() {
-        flags.forEach(element => {
-            element.classList.remove("selected");
-        });
-        element.classList.add("selected");
-    });
-})
+    
 
-$('.carousel').carousel().pause = "hover"
+localStorage.getItem("theme") === null ? localStorage.setItem("theme", "light") : document.querySelector("html").setAttribute("data-bs-theme", localStorage.getItem("theme"));
+
+
+themeswitch.addEventListener('click', function() {
+    document.querySelector("html").setAttribute("data-bs-theme", document.querySelector("html").getAttribute("data-bs-theme") === "dark" ? "light" : "dark");
+    localStorage.setItem("theme", document.querySelector("html").getAttribute("data-bs-theme"));
+});
+
+
+function getRepoStat(component, name) {
+    console.log(component + "hey");
+    let url = `https://api.github.com/repos/hitokii/${name}/languages` 
+    fetch(url).then(data => data.json()).then(res => {
+        let total = 0;
+        for (let i in res) {
+            if (i == "message") {
+                console.log(res)
+                console.log("Error Fetching : " + component.getAttribute("name"));
+                return
+            }
+            total += res[i];
+        }
+        for (let i in res) {
+            component.innerHTML += `<p class=\"card-text\">-${i}</p><div class=\"progress\">  <div class=\"progress-bar ${i}-language\" role=\"progressbar\" style=\"width: ${res[i] / total * 100}%;\" aria-valuenow=\"${res[i] / total * 100}\" aria-valuemin=\"0\" aria-valuemax=\"100\">${Math.round(res[i] / total * 100)}%</div></div>`
+        }
+    })
+}
+
+document.querySelectorAll(".repo").forEach((repo) => {
+    getRepoStat(repo, repo.getAttribute("name"));
+});
