e9b324debcc2d2e893a1d0c8b25a136d09b6ebb3
Hitokii <max@hitoki.dev>
2024-07-02 12:14:09 +0200

    homepage done

--- scripts/Script.js
+++ scripts/Script.js
@@ -1,30 +1,10 @@
-let devtech = document.querySelector('#devtech');
+window.addEventListener('popstate', react);
 
-let index = 0;
-async function typeWriter() {
-    let techs = ["Javascript", "Typescript", "Java", "C++"]
-    await removeWord();
+const pushUrl = (href) => {
+  history.pushState({}, '', href);
+  window.dispatchEvent(new Event('popstate'));
+};
 
-    let word = techs[index];
-
-    setTimeout(() => {
-        devtech.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16); 
-        for (let i = 0; i < word.length; i++) {
-            const element = word[i];
-            setTimeout(() => {
-                devtech.innerHTML += element;
-            }, 100 * i + Math.random() * 100);
-        }
-    }, 1500);
-    index == techs.length - 1 ? index = 0 : index++;
+function react() {
+    
 }
-
-async function removeWord() {
-    for (let i = 0; i < devtech.innerHTML.length; i++) {
-        setTimeout(() => {
-            devtech.innerHTML = devtech.innerHTML.slice(0, -1);
-        }, 100 * i + Math.random() * 100);
-    }
-}
-typeWriter();
-setInterval(() => typeWriter(), 5000);
