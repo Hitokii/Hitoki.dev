386ed83c9aaa341f04a2905135d417d448313e1a
hitoki <max@hitoki.dev>
2024-07-02 20:09:10 +0200

    oui

--- scripts/Script.js
+++ scripts/Script.js
@@ -2,9 +2,38 @@ window.addEventListener('popstate', react);
 
 const pushUrl = (href) => {
   history.pushState({}, '', href);
-  window.dispatchEvent(new Event('popstate'));
+  window.dispatchEvent(new Event('popstate'), { url: href });
 };
 
-function react() {
-    
+document.querySelector(document.location.hash).hidden = false;
+
+function react(href) {
+  // href.target.location.hash
+    let pages = ["#home", "#about"]
+    console.log(href.target.location.hash);
+    pages.forEach(page => {
+      document.querySelector(page).style.animation = "fadeOUT 1s forwards";
+      setTimeout(() => {
+        document.querySelector(page).hidden = true;
+        if (href.target.location.hash == page) {
+          document.querySelector(page).style.animation = "fadeIN 1s forwards";
+          document.querySelector(page).hidden = false;
+        }
+      }, 1000);
+    });
+
 }
+
+
+function getAge() {
+  let birthDate = new Date("2002-12-14");
+  let currentDate = new Date();
+  let age = currentDate.getFullYear() - birthDate.getFullYear();
+  let m = currentDate.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m >= 0 && currentDate.getDate() < birthDate.getDate())) {
+    age--;
+  }
+  return age;
+}
+
+document.querySelector("#age").innerText = getAge();
